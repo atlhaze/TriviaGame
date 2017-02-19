a7cdbcd29291c93e4a2978c917aa1756bad1d81e
atlhaze <mskopell@gmail.com>
2017-02-19 03:19:17 -0500

    fmfl

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,35 +1,119 @@
+var intervalId;
+var timeLeft = 120 ;
+
+var numCorrect = 0;
+var numAnswered  = 0;
+var numUnanswered = 0;
+
+
 $( document ).ready(function(){
-	var time = [];
 
-	$('#startStar').click(function() { 
-		setTimeout(function() { ... }, 12000); });
+	$('.game').hide();
+	$('#questions').hide();
+	$('#gameOver').hide();
 
+	$('#startStar').click(function(){
+
+    	startGame(); 
+    	runClock();
+
+    });
+
+
+	// $('#questions').change(function () {
+
+	// 	checkState();
+
+ //  	})
+
+
+	$('#questions').change(function () {
+
+	    var numCorrect = 0;
+	    var numAnswered  = 0;
+
+	    $("input:checked").each(function() {
+
+ 
+	      var isChecked = this.checked;
+	 	  var correct = this.value === "yes"? true : false;
+		
+	      if (correct){
+	      	numCorrect++;
+	      };
+
+	     numAnswered++;
+	    if (numAnswered === 8){
+	    	endGame(numCorrect)
+	    }
+
+	    });
+  	})
+
+});	
+
+
+function runClock(){
+
+	timeLeft = 120 ;
+
+	clearInterval(intervalId);
+	intervalId = setInterval(function(){ 
+		var minutes = parseInt(timeLeft / 60);
+		var seconds = parseInt(timeLeft % 60);
+		minutes = minutes < 10 ? "0" + minutes : minutes;
+		seconds = seconds < 10 ? "0" + seconds : seconds;
+		$('#time').html(minutes + ":" + seconds);
+
+		if (--timeLeft < 0){
+			clearInterval(intervalId);
+			endGame(checkState());
+			};
+		}, 1000);
+
+};
+
+function checkState(){
+
+	    $("input:checked").each(function() {
+
+	      var isChecked = this.checked;
+	 	  var correct = this.value === "yes"? true : false;
+		
+	      if (correct){
+	      	numCorrect++;
+	      };
+
+	     numAnswered++;
+	    if (numAnswered === 8){
+	    	endGame(numCorrect)
+	    }
+
+
+	    });
+	    return numCorrect;
+
+};
+
+function startGame(){
 	
-	$('#startStar').on("click", function(){
-		$('.game').show();
-		$('.start').hide();
- };
+	$('.start').hide();
+	$('.game').show();
+	$('#questions').show();
 
-//  setTimeout(function(){ alert("Hello"); }, 3000);
-
-// var timeout;
-
-// function timeout_trigger() {
-//     $("#time").innerHTML = 'The timeout has been triggered';
-// }
+};
 
 
-//  $("#start").click( function(){
-//    var counter = 5;
-//    setInterval(function() {
-//      counter--;
-//       if (counter >= 0) {
-//          span = document.getElementById("count");
-//          span.innerHTML = counter;
-//       }
-//       if (counter === 0) {
-//          alert('sorry, out of time');
-//          clearInterval(counter);
-//        }
-//      }, 1000);
-// });
+function endGame(numCorrect){
+
+
+	$('#questions').hide();
+	$('#gameOver').show();
+
+	$('#totalAnswersCorrect').html("Total Correct:" + " " + numCorrect);
+	$('#totalAnswersIncorrect').html("Total Incorrect:" + (8 - numCorrect));
+
+	clearInterval(intervalId);
+
+
+};
